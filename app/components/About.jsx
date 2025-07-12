a67df587f1bfc3bc2984bbf85df4ecd718d7887c
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-07-12 21:51:57 +0530

    added about me page

---
+++ app/components/About.jsx
@@ -0,0 +1,51 @@
+import { assets, infoList,toolsData } from '@/assets/assets'
+import React from 'react'
+import Image from 'next/image'
+
+const About = () => {
+  return (
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-15'>
+        <h4 className='text-center mb-2 text-lg font-Ovo' >
+            Introduction
+        </h4>
+        <h2 className='text-center text-5xl font-Ovo'>
+            About Me
+        </h2>
+
+        <div className='flex w-full flex-col lg:flex-row items-center gap-20 mt-7'>
+            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+                <Image src={assets.user_image} alt="" className='w-full rounded-3xl' />
+            </div>
+            <div className=''>
+                <p className='mb-7 max-w-2xl font-Ovo'>
+                     Passionate and self-driven Computer Science student with a strong foundation in full-stack web development. Skilled in JavaScript, React, Node.js, MongoDB, and Tailwind CSS. I enjoy building clean, responsive applications and constantly learning new technologies. Currently focused on improving my DSA skills and exploring backend architecture, AI, and cloud computing.
+                </p>
+
+                <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+                    {infoList.map(({icon,icon_dark,title,description},index) => (
+                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500'
+                             key={index} >
+                                <Image src={icon} alt={title} className='w-7 mt-1' />
+                                <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                                <p>{description}</p>
+                        </li>
+                    ))}
+                </ul>
+
+                <h4 className='my-2 text-gray-700 font-Ovo'>Tools I use</h4>
+
+                <ul className='flex items-center gap-3 sm:gap-5'>
+                    {toolsData.map((tool,index) => (
+                        <li key={index} className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500'>
+                            <Image src={tool} alt='tool' className='w-5 sm:w-7' />
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { assets } from '../../assets/assets'
 import Image from 'next/image'
 
 const Navbar = () => {
+
+    const[isScroll, setIsScroll] = React.useState(false);
     const sideMenuref = React.useRef();
     const openMenu = () => {
         sideMenuref.current.style.transform = 'translateX(0rem)';
@@ -10,19 +12,31 @@ const Navbar = () => {
     const closeMenu = () => {
         sideMenuref.current.style.transform = 'translateX(16rem)';
     }
+    React.useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(scrollY > 50) {
+                setIsScroll(true);
+                
+            }
+            else {
+                setIsScroll(false);
+            }
+        }
+        )}, []);
+        
   return (
     <>
         <div className='fixed top-0 right-0  w-11/12 -z-50 translate-y-[-80%]'>
             <Image src={assets.header_bg_color} alt="" className='w-full'/>
         </div>
 
-        <nav className='w-full fixed  px-5 lg:px-8 xl:px-[8%]  py-4 flex items-center  justify-between z-50 '>
+        <nav className={`w-full fixed  px-5 lg:px-8 xl:px-[8%]  py-4 flex items-center  justify-between z-50 ${isScroll ? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm' : ''}`}>
             
             <a href="#top" className="logo">
                 <Image src={assets.logo} alt='Logo' className='w-28 cursor-pointer mr-14' />
             </a>
 
-            <ul className='hidden md:flex items-center gap-5 lg:gap-6 rounded-full px-10 py-3 bg-white shadow-sm bg-opacity-50'>
+            <ul className={`hidden md:flex items-center gap-5 lg:gap-6 rounded-full px-10 py-3 ${isScroll?"":"bg-white shadow-sm bg-opacity-50"} `}>
                 <li><a className='font-Ovo text-base' href="#top">Home</a></li>
                 <li><a className='font-Ovo text-base' href="#about">About Me</a></li>
                 <li><a className='font-Ovo text-base' href="#project">Projects</a></li>
